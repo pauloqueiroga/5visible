28c5b80cfc57d46fdd125f17c913d08c4b8f9d26
Paulo Queiroga <890636+pauloqueiroga@users.noreply.github.com>
2021-09-15 19:06:38 -0700

    Added some documentation for stacks

--- golang/stack.go
+++ golang/stack.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 )
 
+// Brick identifies each of the players in the game.
 type Brick int
 
 const (
@@ -12,25 +13,31 @@ const (
 	Brick1
 )
 
+// stack represents the state of each stack on the board of 5-visible.
 type stack struct {
 	bricks  []Brick
 	cap     int
 	blocked bool
 }
 
+// newStack allocates a new stack and initializes it with the correct
+// capacity
 func newStack(cap int) *stack {
 	s := stack{bricks: make([]Brick, 0), cap: cap}
 	return &s
 }
 
+// canPop returns if the stack can be used as a "source" of bricks.
 func (s *stack) canPop() bool {
 	return !s.blocked && len(s.bricks) > 0
 }
 
+// canPush returns if the stack stil has capacity available for one more brick.
 func (s *stack) canPush() bool {
 	return s.cap > len(s.bricks)
 }
 
+// push places a Brick at the top of the stack, and marks the stack as blocked.
 func (s *stack) push(b Brick) (*stack, error) {
 	if len(s.bricks) == s.cap {
 		return s, errors.New("can't push, stack is full")
@@ -45,6 +52,7 @@ func (s *stack) push(b Brick) (*stack, error) {
 	return s, nil
 }
 
+// peek returns the Brick at the top of the stack, without removing it.
 func (s *stack) peek() (Brick, error) {
 	if len(s.bricks) == 0 {
 		return NotABrick, errors.New("stack is empty")
@@ -53,6 +61,7 @@ func (s *stack) peek() (Brick, error) {
 	return s.bricks[len(s.bricks)-1], nil
 }
 
+// pop removes tha Brick at the top of the stack and returns it.
 func (s *stack) pop() (Brick, error) {
 	if !s.canPop() {
 		return NotABrick, errors.New("stack is blocked or empty")
@@ -62,8 +71,4 @@ func (s *stack) pop() (Brick, error) {
 	popped := s.bricks[n]
 	s.bricks = s.bricks[:n]
 	return popped, nil
-}
-
-func (s *stack) hashcode() int64 {
-	return 0
 }
