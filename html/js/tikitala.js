44215a03ac900d20774386a662d9e3de29f6b211
Paulo Queiroga <890636+pauloqueiroga@users.noreply.github.com>
2022-08-27 13:28:27 -0500

    added ids to html elements, started playing with JS viewmodel

--- html/js/tikitala.js
+++ html/js/tikitala.js
@@ -1,41 +1,43 @@
-/*
-            <table id="placeholders">
-                <tbody>
-                    <tr>
-                        <td class="stack zero-tall"></td>
-                        <td class="stack one-tall chip-0"></td>
-                        <td class="stack two-tall chip-0"></td>
-                    </tr>
-                    <tr>
-                        <td class="stack three-tall chip-1"></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-
-*/
 var board = {
+    chipFaceNone : $('#chipface-none').clone(),
+    chipFace0 : $('#chipface-0').clone(),
+    chipFace1 : $('#chipface-1').clone(),
     start : function () {
-        $(".top-chip-none").html('')
-        var $chipFace0 = $('#chipface-0').clone()
-        var $chipFace1 = $('#chipface-1').clone()
-        $(".top-chip-0").html($chipFace0)
-        $(".top-chip-1").html($chipFace1)
-        $(".zero-tall").html("")
-        $(".one-tall").html(pileUp(1))
-        $(".two-tall").html(pileUp(2))
-        $(".three-tall").html(pileUp(3))
-        $(".four-tall").html(pileUp(4))
-        $(".five-tall").html(pileUp(5))
-        $(".six-tall").html(pileUp(6))
-        $(".seven-tall").html(pileUp(7))
-        $(".eight-tall").html(pileUp(8))
+        this.stash = [8, 8];
+        this.stackCount = [ 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        this.stackTop =   [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+        stackLock = [false,false,false,false,false,false,false,false,false];
+
+        for (let i = 1; i <= 9; i++) {
+        }
+        this.updateViewModel();
+        this.refreshView();
+    },
+    updateViewModel : function () {
+        $('#hidden-0-0').removeClass().addClass('tall-'+this.stash[0]);
+        $('#hidden-0-1').removeClass().addClass('tall-'+this.stash[1]);
+
+        if (this.stash[0] > 0) {
+            $('#visible-0-0').removeClass().addClass('top-chip-0');
+        } else {
+            $('#visible-0-0').removeClass().addClass('top-chip-none');
+        }
+
+        if (this.stash[1] > 0) {
+            $('#visible-0-1').removeClass().addClass('top-chip-1');
+        } else {
+            $('#visible-0-1').removeClass().addClass('top-chip-none');
+        }
+
+    },
+    refreshView : function () {
+        $(".top-chip-none").html(this.chipFaceNone)
+        $(".top-chip-0").html(this.chipFace0)
+        $(".top-chip-1").html(this.chipFace1)
+        $(".tall-0").html("")
+        for (let i = 1; i <= 8; i++) {
+            $(".tall-" + i).html(pileUp(i))
+        }
     },
     clear : function () {
     }
